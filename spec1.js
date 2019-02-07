76e36919c43a4708a35d4dc9a535b914f0917658
Juan Vivas <juanlvo@gmail.com>
2019-02-07 12:20:01 +0100

    first commit

---
+++ spec1.js
@@ -0,0 +1,21 @@
+/**
+ * Section 2 Lecture 7
+ * 
+ * @author: vivasoju
+ * 
+ * Steps:
+ * 1. declare the test suite
+ * 2. declare parameters and test suite
+ * 3. declaration of it (test case)
+ * 4. declaration of the test case name
+ */
+describe('Protractor early steps',function() {
+	it('test case 1 - basic progam', function(){
+		//protractor code
+		
+	});
+	
+	it('test case 2 - close browser', function(){
+		
+	});
+});
