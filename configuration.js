607dffb0d55a6882ada78c4b40baf8d39e5b8d07
Juan Vivas <juanlvo@gmail.com>
2019-02-07 16:38:34 +0100

    Section 6 Lecture 35 running protractor with firefox

--- configuration.js
+++ configuration.js
@@ -2,6 +2,9 @@
  * Section 2 Lecture 9
  * Importance of configuration file in protractor framework
  * 
+ * Section 6 Lecture 35
+ * Running protractor test on firefox and internet explorer (not working with the last driver :( )
+ * 
  * @author: vivasoju
  */
 exports.config = {
@@ -9,6 +12,14 @@ exports.config = {
   //specs: ['dummytest.js'],
   //specs: ['spec1.js'],
   specs: ['ElementBasics.js'],
+   capabilities: {
+    'browserName': 'firefox'
+  }, 
+/*   capabilities: {
+    'browserName': 'internet explorer',
+    'platform': 'ANY',
+    'version': '11'
+    },  */
   onPrepare : function(){
     browser.manage().window().maximize();
   }
