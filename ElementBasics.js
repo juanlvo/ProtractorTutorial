5ada4b21553e1540099504139086fc0a1ca67918
Juan Vivas <juanlvo@gmail.com>
2019-02-07 14:44:55 +0100

    feat: Section 6 Lecture 30 understanding local variables in protractor

---
+++ ElementBasics.js
@@ -0,0 +1,11 @@
+/***
+ * Section 6 Lecture 30
+ * Understanding gloval variable in protractor
+ * 
+ * @author: vivasoj
+ */
+describe('element basics',function(){
+    it('test case 1', function(){
+        browser.get('http://juliemr.github.io/protractor-demo/');
+    });
+});
