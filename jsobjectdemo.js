bb8c2fdb73aab8adb169e32128e54d55bdd22960
Juan Vivas <juanlvo@gmail.com>
2019-02-24 22:49:18 +0100

    feat: Section 17 Lecture 103 Java script object demo

---
+++ jsobjectdemo.js
@@ -0,0 +1,13 @@
+/***
+ * Section 17 Lecture 103
+ * Java script object demo
+ */
+function car() {
+    this.color = "red",
+    this.engine = "turbo";
+    this.brand = "BMW";
+
+    this.getModel() = function() {
+        console.log("this is a 2020 model");
+    }
+}
