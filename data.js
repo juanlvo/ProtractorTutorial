619b334753a0c7c3fcf47100caa52883f91c1ad7
Juan Vivas <juanlvo@gmail.com>
2019-03-07 14:33:33 +0100

    Section 20 Lecture 113 introduction to datadriven testing with jasmine

---
+++ data.js
@@ -0,0 +1,13 @@
+/***
+ * Section 20 Lecture 113
+ * Introduction to datadriven testing with jasmine
+ * 
+ * @author juanlvo
+ */
+module.exports.datadriver = {
+    firstinput:"2",
+    secondinput:"3",
+    result:"5"
+}
+
+console.log(datadriver.result);
